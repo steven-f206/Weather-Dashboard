060a0cb9a07a9a51d85072272659dc59a7b6936c
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-01-18 21:44:10 -0500

    added city jSON and city list JSON from cityjson

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-  
-    $("button").click(function(){
-      $.ajax({url: "https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=428bbab3989b31eb5f6dd40e0559cbeb", success: function(result){
-        $("div").html(result);
-      }});
-    });
-  });
